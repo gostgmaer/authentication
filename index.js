2569a34fdadd2ef1a0ad04b8f935c3b796ff02a1
Kishor Sarkar <kishor.sarkar@inadev.com>
2023-09-30 14:24:08 +0530

    updated version

--- index.js
+++ index.js
@@ -7,16 +7,25 @@ const authRouter = require("./routes/auth");
 app.use(cors());
 app.use(express.json());
 app.use("/api", authRouter);
+
+app.get("/", (req, res) => {
+  res.send("APP is working!");
+});
+
+app.get("/api", (req, res) => {
+  res.send("API is working!");
+});
+
 //Port and Connect to DB
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
     app.listen(port, () => {
-         console.log(`Server is running on port ${port}`);
+      console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
-      console.log("error =>", error);
+    console.log("error =>", error);
   }
 };
 start();
