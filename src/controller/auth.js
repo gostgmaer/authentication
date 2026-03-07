175471bd329bc0de0ab822d714f5cffd4d4554fc
gostgmaer <kishor81160@gmail.com>
2026-03-08 02:12:58 +0530

    refactor: replace bcrypt with bcryptjs in package.json and relevant files

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -7,7 +7,7 @@ const {
 const { dbUrl, jwtSecret, refressSecret } = require("../config/setting");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const sessionStore = require("../db/session");
 const createMailOptions = require("../mail/mailOptions");
 const transporter = require("../mail/mailTransporter");
